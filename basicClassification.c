6c2a835c5b93b7d4db061843deddbb4ca3134b87
OriHoward <ori.howard@gmail.com>
2021-10-30 13:55:29 +0300

    added functions

--- basicClassification.c
+++ basicClassification.c
@@ -1,11 +1,38 @@
 #include "NumClass.h"
+#include <math.h>
 
 
-
-
-int isArmstrong(num) {
-    return -3;
+int isPrime(int num) {
+    int i;
+    for (i = 2; i <= num / 2; i++) {
+        if (num % i != 0) {
+            continue;
+        } else{
+            return 0;
+        }
+    }
+    return 1;
 }
+
+int isStrong(int num) {
+    int sum = 0;
+    int newNum = num;
+    while(newNum > 0) {
+        sum += factorial(newNum%10);
+        newNum = newNum/10;
+    }
+    return sum == num;
+}
+
+int factorial (int num) {
+    int i;
+    for(i=num-1; i>0; i--) {
+        num *= i;
+    }
+    return num;
+}
+
+
 
 
 
