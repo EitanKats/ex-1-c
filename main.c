c46087127faefc17d5b10962359f06be2db3fcb1
OriHoward <ori.howard@gmail.com>
2021-11-03 15:05:11 +0200

    Submition attempt

--- main.c
+++ main.c
@@ -8,33 +8,36 @@ int main() {
     printf("enter 2 positive numbers: \n");
     scanf("%d %d", &num1, &num2);
 
-    if(num1 <=0 || num2<=0){
+    if (num1 <= 0 || num2 <= 0) {
         printf("both numbers weren't positive: \n");
         return 0;
     }
 
+    printf("\n The Armstrong numbers are: ");
+    for (int i = num1; i <= num2; i++) {
+        if (isArmstrong(i) == 1) {
+            printf("%d , ", i);
+        }
+    }
+
     printf("\n The Palindromes are: ");
-    for (int i = num1; i <= num2 ; i++) {
+    for (int i = num1; i <= num2; i++) {
         if (isPalindrome(i) == 1) {
             printf("%d , ", i);
         }
     }
+
     printf("\n The Prime numbers are: ");
-    for (int i = num1; i <= num2 ; i++) {
+    for (int i = num1; i <= num2; i++) {
         if (isPrime(i) == 1) {
             printf("%d , ", i);
         }
     }
+
     printf("\n The Strong numbers are: ");
 
-    for (int i = num1; i <= num2 ; i++) {
+    for (int i = num1; i <= num2; i++) {
         if (isStrong(i) == 1) {
-            printf("%d , ", i);
-        }
-    }
-    printf("\n The Armstrong numbers are: ");
-    for (int i = num1; i <= num2 ; i++) {
-        if (isArmstrong(i) == 1) {
             printf("%d , ", i);
         }
     }
